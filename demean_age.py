c893dbe820b213261678373047fd8b8ce10adcab
Dave Warren <davideugenewarren@gmail.com>
2017-09-04 16:50:50 -0500

    Adding assertions.

--- demean_age.py
+++ demean_age.py
@@ -4,6 +4,9 @@ age = np.loadtxt("participants.tsv", skiprows=1, usecols=3)
 
 mean_age = sum(age)/len(age)
 
+assert mean_age < 100
+assert mean_age > 10
+
 np.savetxt("demeaned_age.txt", age-mean_age)
 
 print("done!!!")
